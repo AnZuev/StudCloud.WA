5acca477ff8131d71c9dff8fedafce14837428f0
Mtdr <paul.osmolovsky@gmail.com>
2016-07-31 22:08:37 +0300

    0.0.0b(6)

--- routes/authorize/confirmMail.js
+++ routes/authorize/confirmMail.js
@@ -2,14 +2,17 @@
 let sso = require('@anzuev/studcloud.sso');
 let log = require(appRoot + '/libs/log');
 let uams = require('@anzuev/studcloud.uams');
-let rds = require('@anzuev/studcloud.rds');
+// let rds = require('@anzuev/studcloud.rds');
 
 function* a(){
     try {
-        let mail = this.request.body.mail;
-        let key = this.request.body.key;
-        let confirmation = yield sso.confirmMail(mail,key);
-        return confirmation;
+        let data = {
+            password: this.request.body.key,
+            mail: this.request.body.mail
+        };
+        let res = yield sso.user.confirmMail(data);
+        log.info(res);
+        return res;
     } catch (err) {
         log.info(err + "ERROR");
     }
